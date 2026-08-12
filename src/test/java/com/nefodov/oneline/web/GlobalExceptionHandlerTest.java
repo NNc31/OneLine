1782ea445b7ce1911637da73c633ac8953ecd351
NNc31 <nefodov.nazar@proton.me>
2026-08-12 15:02:38 +0300

    Remove unused import

--- src/test/java/com/nefodov/oneline/web/GlobalExceptionHandlerTest.java
+++ src/test/java/com/nefodov/oneline/web/GlobalExceptionHandlerTest.java
@@ -6,7 +6,6 @@ import com.nefodov.oneline.exception.StorageException;
 import com.nefodov.oneline.exception.TooManyRequestsException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
